9094d6050d56995d0b0b74700938856a1df0b6c1
rolanveroncruz <rolanveroncruz@gmail.com>
2023-06-11 19:47:04 +0800

    working on detail page

---
+++ lib/pages/food/popular_food_detail.dart
@@ -0,0 +1,71 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:rewardsplus_ecomm/utils/dimensions.dart';
+
+import '../../utils/colors.dart';
+import '../../widgets/app_column.dart';
+import '../../widgets/app_icon.dart';
+import '../../widgets/icon_and_text_widget.dart';
+import '../../widgets/text_widgets.dart';
+
+class PopularFoodDetail extends StatelessWidget {
+  const PopularFoodDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Positioned(
+            left: 0,
+            right: 0,
+            child: Container(
+              width: double.maxFinite,
+              height: Dimensions.popularFoodImgSize,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage("assets/image/food0.png"))),
+            )),
+        Positioned(
+            top: Dimensions.height45,
+            left: Dimensions.width20,
+            right: Dimensions.width20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                AppIcon(icon: Icons.arrow_back_ios),
+                AppIcon(icon: Icons.shopping_cart_outlined)
+              ],
+            )),
+        Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: Dimensions.popularFoodImgSize - 30,
+            child: Container(
+                padding: EdgeInsets.only(
+                    left: Dimensions.width20,
+                    right: Dimensions.width20,
+                    top: Dimensions.height20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(Dimensions.radius20),
+                      topRight: Radius.circular(Dimensions.radius20),
+                    ),
+                    color: Colors.white),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    AppColumn(text: "Chinese Side"),
+                    SizedBox(
+                      height: Dimensions.height20,
+                    ),
+                    BigText(text: "Introduce")
+                  ],
+                )))
+      ],
+    ));
+  }
+}
